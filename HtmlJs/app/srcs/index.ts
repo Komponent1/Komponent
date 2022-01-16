cff04685aeef3935432c7ea875c5e5aef726532f
seo2im <seo2im6492@gmail.com>
2022-01-16 15:41:10 +0900

    feat: js scrollspy update

--- HtmlJs/app/srcs/index.ts
+++ HtmlJs/app/srcs/index.ts
@@ -2,8 +2,11 @@ import './style.css'
 
 import contextMenu from './contextMenu/contextMenu';
 import { makeDiv } from './utils';
+import scrollSpy from './scrollSpy/scrollSpy';
 
 const app: HTMLElement|null = document.getElementById("root");
-const ContextMenu = makeDiv("Context Menu" ,contextMenu(document.body, 5));
+const ContextMenu = makeDiv("Context Menu", contextMenu(document.body, 5));
+const ScrollSpy = makeDiv("ScrollSpy", scrollSpy(5));
 
 app.appendChild(ContextMenu);
+app.appendChild(ScrollSpy);
---
+++ HtmlJs/app/srcs/scrollSpy/scrollSpy.ts
@@ -0,0 +1,68 @@
+import './scrollSpy.css';
+
+function scrollSpy(num: number): HTMLDivElement {
+  const wrapper = document.createElement("div");
+  wrapper.classList.add("scrollspy_wrapper");
+  const nav = document.createElement("div");
+  nav.classList.add("scrollspy_nav");
+  wrapper.appendChild(nav);
+
+  const navitems = [];
+  const items = [];
+  for(let i = 0; i < num; i++) {
+    let item = document.createElement("div");
+    item.classList.add("scrollspy_item");
+    wrapper.appendChild(item);
+    items.push(item);
+    
+    let navitem = document.createElement("div");
+    navitem.classList.add("scrollspy_navitem");
+    navitem.style.setProperty("--num", num + '');
+    nav.appendChild(navitem)
+    navitems.push(navitem);
+  }
+
+  const getOffset = (() => {
+    let offsetTop = 0;
+    let res = [];
+
+    return () => {
+      if (window.innerHeight === offsetTop) {
+        return res;
+      }
+      offsetTop = window.innerHeight;
+      res = items.map(e => {
+        const [ offset, clientHeight ] = [ e.offsetTop, e.clientHeight ];
+        return [ offset - clientHeight / 2, offset + clientHeight / 2 ];
+      })
+      
+      return res;
+    }
+  })();
+
+  window.addEventListener("scroll", (e: any) => {
+    const { scrollTop } = e.target.scrollingElement;
+
+    const idx = getOffset().findIndex(([from, to]) => (
+      scrollTop >= from && scrollTop < to
+    ));
+    navitems.forEach((navitem, i) => {
+      if (i === idx) navitem.classList.add("select");
+      else navitem.classList.remove("select");
+    })
+  })
+
+  nav.addEventListener("click", e => {
+    e.stopPropagation();
+    const idx = navitems.indexOf(e.target);
+
+    items[idx].scrollIntoView({
+      block: 'start',
+      behavior: 'smooth'
+    })
+  });
+
+  return wrapper;
+}
+
+export default scrollSpy;
