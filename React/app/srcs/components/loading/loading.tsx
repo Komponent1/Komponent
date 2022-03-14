ab4844f8ec3adac980cc5d6812d22c2cd60f28d1
seo2im <seo2im6492@gmail.com>
2022-03-14 01:41:10 -0700

    fix: fixing loading in react

--- React/app/srcs/components/loading/loading.tsx
+++ React/app/srcs/components/loading/loading.tsx
@@ -5,14 +5,27 @@ type Prop = {
   type?: 'linearspin' | 'delayspin' | 'line'
 }
 
+const typeCheck = (type) => {
+  switch(type) {
+    case 'linearspin':
+      return (<style.linearspin className='kui_loading_linearspin'/>);
+    case 'delayspin':
+      return (<style.delayspin className='kui_loading_delayspin'/>);
+    case 'line':
+      return (
+        <style.lines className='kui_loading_lines'>
+          <style.line className='kui_loading_line' delay={0} />
+          <style.line className='kui_loading_line' delay={0.2} />
+          <style.line className='kui_loading_line' delay={0.4} />
+        </style.lines>
+      )
+  }
+}
+
 const Loading = ({ type = 'linearspin' }: Prop) => {
   return (
-    <style.div>
-      <style.lines>
-        <style.line delay={0} />
-        <style.line delay={0.2} />
-        <style.line delay={0.4} />
-      </style.lines>
+    <style.div className='kui_loading'>
+      {typeCheck(type)}
     </style.div>
   )
 };
