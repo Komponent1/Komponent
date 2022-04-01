55337c42cb292e7222aa5c199357ab7df938bd9e
seo2im <seo2im6492@gmail.com>
2022-04-01 21:59:29 +0900

    fix: save point

--- publish/config/webpack.config.js
+++ publish/config/webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
     output: {
       path: path.join(__dirname, '../dist'),
       filename: 'index.js',
+      libraryTarget: 'commonjs'
     },
     mode: 'production',
     resolve: {
