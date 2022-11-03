5e85502f7a1464a29b8da11f6e5722981ed53816
seo2im <seo2im6492@gmail.com>
2022-11-03 18:16:13 +0900

    📝 areainput 스토리 추가

--- React/app/component/form/areainput/areainput.tsx
+++ React/app/component/form/areainput/areainput.tsx
@@ -3,12 +3,19 @@ import { FormControl } from '../useFormControl';
 import * as S from './style';
 
 export type AreaInputProps = HTMLAttributes<HTMLTextAreaElement> & {
+  /** textarea 값 변수(state) */
   control: FormControl<string, HTMLTextAreaElement>,
+  /** element id(라벨과 동일하게 설정), 아이디는 모든 폼요소가 다르게 설정되어야 합니다. */
   id?: string;
+  /** 플레이스 홀더 */
   placeholder?: string;
+  /** 에어리어 너비 */
   width?: number | string;
+  /** 에어리어 높이(텍스트 줄수) */
   rows?: number;
+  /** 디자인 요소 */
   design?: S.SelectDesign;
+  /** 폰트 크기 및 패딩 값 조작 */
   scale?: S.FormScalesType;
 };
 function AreaInput({
--- React/app/example/src/index.tsx
+++ React/app/example/src/index.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  AreaInput, Form, useForm,
-} from '../../component/form';
 
 function App() {
-  const { submit, controls } = useForm([
-    { id: 'test', type: 'area-input', controlOption: {} },
-  ], () => {});
-
   return (
-    <Form submit={submit}>
-      <AreaInput
-        width={200}
-        design='underline'
-        control={controls.test}
-      />
-    </Form>
+    <div>
+
+    </div>
   );
 }
 
